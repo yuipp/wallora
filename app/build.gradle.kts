30ea7c4eb03d17768f795ec88069472e6f142ba3
yuipp <48102302+yuipp@users.noreply.github.com>
2026-09-16 10:01:02 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = 7
-        versionName = "1.6"
+        versionName = "1.61"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
