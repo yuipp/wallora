e1a177916ca69dff63675088ea331baf14bb67b3
yuipp <48102302+yuipp@users.noreply.github.com>
2026-09-16 08:06:40 +0300

    Update build.gradle.kts
    V1. 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.wallora.app"
         minSdk = 31
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
